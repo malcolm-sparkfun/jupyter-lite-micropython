5ad854252c8407cc6a054301922ec51ad39e774b
Malcolm McKellips <malcolm.mckellips@sparkfun.com>
2025-04-17 16:01:22 -0600

    dbg

--- kernel/src/services/FirmwareService.ts
+++ kernel/src/services/FirmwareService.ts
@@ -29,7 +29,7 @@ export class FirmwareService {
   }
 
   private async unzipStreamToVariable(stream: ReadableStream<Uint8Array>): Promise<{ [filename: string]: Uint8Array }> {
-    print("In unzipStreamToVariable method.");
+    console.log('Unzipping stream...');
     const reader = stream.getReader();
     let chunks = [];
     while (true) {
